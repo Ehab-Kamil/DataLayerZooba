3f7fc38892c6dfe114fa79ec48b688d780ee99d5
yoka132 <aya.farag3011@gmail.com>
2016-05-31 17:12:35 +0200

    vehicles

--- DataLayerZooba/src/main/java/com/mycompany/datalayerzooba/MainTest.java
+++ DataLayerZooba/src/main/java/com/mycompany/datalayerzooba/MainTest.java
@@ -5,8 +5,10 @@
  */
 package com.mycompany.datalayerzooba;
 
+import dao.MakeDao;
 import dao.UserDao;
 import java.util.ArrayList;
+import pojo.Make;
 import pojo.User;
 
 /**
@@ -18,15 +20,15 @@ public class MainTest {
     
     public static void main(String[] args) {
         
-        User u = new User();
-        u.setUsername("Ehab 1UserName");
-        u.setFirstName("Ehab 1Kamil");
-        u.setEmail("Ehab Email1");
-        u.setPassword("Ehab Pas1sword");
-        u.setSuspended(0);
-//        
-        UserDao uDao = new UserDao();
-      uDao.create(u);
+//        User u = new User();
+//        u.setUsername("Ehab 1UserName");
+//        u.setFirstName("Ehab 1Kamil");
+//        u.setEmail("Ehab Email1");
+//        u.setPassword("Ehab Pas1sword");
+//        u.setSuspended(0);
+////        
+//        UserDao uDao = new UserDao();
+//      uDao.create(u);
 //       User u=new User();
 //       // u.setUsername("aya");
 //        u.setPassword("aya");
@@ -36,6 +38,11 @@ public class MainTest {
 //   User u1= uarr.get(0);
 //        System.out.println("uarr "+uarr.size());
 //        
+MakeDao m1=new MakeDao();
+ArrayList<Make> m=new ArrayList<>(m1.findAll());
+m.stream().forEach((mm) -> {
+    System.out.println("m :"+mm.getName());
+        });
     }
     
 }
