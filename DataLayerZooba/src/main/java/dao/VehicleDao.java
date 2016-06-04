2b1c7263756e60920f5393fbc0b169f5089d9ff1
Riham <Riham@Riham-PC>
2016-06-04 15:01:20 -0700

    vehicles of users

--- DataLayerZooba/src/main/java/dao/VehicleDao.java
+++ DataLayerZooba/src/main/java/dao/VehicleDao.java
@@ -7,8 +7,15 @@ package dao;
 
 import Exceptions.DataAccessLayerException;
 import abstractDao.AbstractDao;
+import abstractDao.HibernateFactory;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import pojo.Vehicle;
+import pojo.VehicleModel;
+import pojo.Year;
 
 /**
  *
@@ -16,8 +23,10 @@ import pojo.Vehicle;
  */
 public class VehicleDao extends AbstractDao<Vehicle> {
 
+    Session session;
     public VehicleDao() {
         super(Vehicle.class);
+        session= HibernateFactory.openSession();
     }
 
     @Override
@@ -43,5 +52,33 @@ public class VehicleDao extends AbstractDao<Vehicle> {
     public List<Vehicle> findByExample(Vehicle t) {
         return super.findByExample(t); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public List<Object[]> getByMake()
+    {
+        List<Vehicle> v=new ArrayList();
+        String hql="select v.name , vm.year, vm.trim, vm.model, ma.name From Vehicle v , VehicleModel vm, Make ma, "
+                + "Model mo where v.vehicleModel.id=vm.id "
+                + "and  mo.make.id=ma.id  and vm.model.id=mo.id";
+        Query query = session.createQuery(hql);
+         List<Object[]> result=query.list();
+         for(Object[] row:result)
+         {
+            String name=(String) row[0];
+            Year y= (Year) row[1];
+            String makename=(String)row[4];
+            System.out.println("Hello "+name+" "+ y.getName()+" "+makename);
+         } 
+         return result;
+    }
 
+    public Vehicle getVehicleByName(String name)
+    {
+        Vehicle v=new Vehicle();
+        String hql="From Vehicle v where v.name=?";
+        Query query = session.createQuery(hql).setString(0, name);
+        
+        v=(Vehicle) query.uniqueResult();
+        HibernateFactory.close(session);
+        return v;
+    }
 }
