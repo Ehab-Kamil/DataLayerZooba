0b448bcbaf2b499ab3c9465055c7a3ace9b1f85a
yoka132 <aya.farag3011@gmail.com>
2016-06-05 06:06:57 +0200

    transfer handler to data zooba layer

--- DataLayerZooba/src/main/java/com/mycompany/datalayerzooba/MainTest.java
+++ DataLayerZooba/src/main/java/com/mycompany/datalayerzooba/MainTest.java
@@ -7,13 +7,17 @@ package com.mycompany.datalayerzooba;
 
 import dao.MakeDao;
 import dao.ModelDao;
+import dao.ModelFeatureValueDao;
 import dao.UserDao;
+import dao.YearDao;
 import facadePkg.DataLayer;
 import pojo.Make;
 import pojo.Model;
 import pojo.Trim;
 import java.util.ArrayList;
+import java.util.List;
 import pojo.Make;
+import pojo.ModelFeaturesValues;
 import pojo.User;
 import pojo.Year;
 
@@ -24,5 +28,11 @@ import pojo.Year;
 public class MainTest {
 
     public static void main(String[] args) {
+//        YearDao yearDao=new YearDao();
+//       List<Year> list= yearDao.getYearByModel("bte5a");
+//        System.out.println("com.mycompany.datalayerzooba.MainTest.main() "+list.get(0).getName());
+pojo.ModelFeaturesValues featuresValues=new ModelFeaturesValues();
+//featuresValues.
+
     }
 }
---
+++ DataLayerZooba/src/main/java/webServicesHandlers/Handler.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webServicesHandlers;
+
+import Exceptions.DataAccessLayerException;
+import abstractDao.HibernateFactory;
+import dao.MakeDao;
+import dao.ModelDao;
+import dao.TrimDao;
+import dao.UserDao;
+import dao.YearDao;
+import facadePkg.DataLayer;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import pojo.Make;
+import pojo.Model;
+import pojo.Trim;
+import pojo.User;
+import pojo.Year;
+
+/**
+ *
+ * @author yoka
+ */
+public class Handler {
+   Session session;
+   UserDao uDao;
+    public Handler(){
+  session= HibernateFactory.openSession();
+             uDao = new UserDao(session);
+    }
+  
+
+     public User login(String user, String pass) {
+        User u = new User();
+        u.setUsername(user);
+        u.setPassword(pass);
+
+        ArrayList<User> uarr = (ArrayList<User>) uDao.findByExample(u);
+        if (uarr.size() > 0) {
+            User u1 = uarr.get(0);
+            return u1;
+        } else {
+            return null;
+        }
+    }
+
+   public boolean userExists(String user) {
+        User u = new User();
+        u.setUsername(user);
+        ArrayList<User> uarr = (ArrayList<User>) uDao.findByExample(u);
+        return uarr.size() > 0;
+    }
+
+    public int register(User u) {
+        uDao.create(u);
+        return 0;
+    }
+
+   public List<Make> getMake() {
+        List<Make> result = new ArrayList<>();
+        MakeDao makeDao = new MakeDao();
+        result = makeDao.findAll();
+        return result;
+    }
+
+    public List<Model> getModelByMake(String make) {
+        ModelDao modelDao = new ModelDao();
+        List<Model> result = modelDao.getModelsByMake(make);
+        return result;
+    }
+
+  public  List<Trim> getTrim(String model, String year) {
+
+        TrimDao trimDao = new TrimDao();
+        return trimDao.getTrimByYearAndModel(model, year);
+
+    }
+
+   public List<Year> getYear(String model) {
+
+        YearDao yearDao = new YearDao();
+        return yearDao.getYearByModel(model);
+        
+    }
+
+  public  boolean addVehicle(String make, String model, String year, String trim) {
+        boolean result = false;
+        try {
+            DataLayer dataLayer = new DataLayer();
+            Make newMake = new Make(make, make);
+            Model newModel = new Model(newMake, model, model);
+            Year newYear = new Year(Integer.parseInt(year));
+            Trim newTrim = new Trim(trim);
+            dataLayer.insertVehicle(newMake, newModel, newYear, newTrim);
+            result = true;
+        } catch (DataAccessLayerException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+  public  User loginByEmail(String email, String pass) {
+        User u = new User();
+        u.setEmail(email);
+        u.setPassword(pass);
+//        uDao = new UserDao();
+        ArrayList<User> uarr = (ArrayList<User>) uDao.findByExample(u);
+         if (uarr.size() > 0) {
+            User u1 = uarr.get(0);
+            return u1;
+        } else {
+            return null;
+        }
+    }
+//    String forgetPassword()
+//    {
+//        
+//    }
+
+  public  boolean emailExists(String email) {
+        User u = new User();
+        u.setEmail(email);
+        ArrayList<User> uarr = (ArrayList<User>) uDao.findByExample(u);
+        return uarr.size() > 0;
+    }
+
+}
