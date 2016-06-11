247093ce7747ad37559bdd9b0f0b5666d756a026
Ehab-Kamil <Ehabkamil2@gmail.com>
2016-06-11 12:57:10 +0200

    Aya Ibrahem

---
+++ DataLayerZooba/src/main/java/dao/DeviceDao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import abstractDao.AbstractDao;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import pojo.Device;
+
+/**
+ *
+ * @author yoka
+ */
+public class DeviceDao extends AbstractDao<Device>{
+    Session  session;
+    public DeviceDao(Session s) {
+         super(Device.class,s);
+    session=s;
+    }
+    public List<Device> getdevicePerUser(int userId)
+    {
+      Criteria crt = session.createCriteria(Device.class, "device").
+                createAlias("device.user", "user")
+                .add(Restrictions.eq("user.id",  userId));
+        List<Device> lst = crt.list();
+
+        return lst;
+    }
+   
+}
