ae01c72639cdbce2a5f552f6f8aa4470af084549
Riham <Riham@Riham-PC>
2016-06-12 14:00:01 -0700

    Riham Modification

--- DataLayerZooba/src/main/java/reham/DataLayer.java
+++ DataLayerZooba/src/main/java/reham/DataLayer.java
@@ -7,7 +7,11 @@ package reham;
 
 import Exceptions.DataAccessLayerException;
 import abstractDao.HibernateFactory;
+import dao.MakeDao;
 import dao.ModelFeatureValueDao;
+import dao.ServiceDAO;
+import dao.ServiceProviderDAO;
+import dao.ServiceProviderServicesDao;
 import dao.VehicleDao;
 import dao.VehicleModelDao;
 import java.io.Serializable;
@@ -15,7 +19,11 @@ import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import pojo.Make;
 import pojo.ModelFeaturesValues;
+import pojo.Service;
+import pojo.ServiceProvider;
+import pojo.ServiceProviderServices;
 import pojo.Vehicle;
 import pojo.VehicleModel;
 
@@ -31,11 +39,19 @@ public class DataLayer implements Serializable {
     VehicleModelDao vmDao;
     Transaction transaction;
     VehicleDao vDao;
+    ServiceProviderDAO sDao;
+    ServiceProviderServicesDao spDao;
+    MakeDao mDao;
+    ServiceDAO serviceDao;
     public DataLayer() {
         session=HibernateFactory.openSession();
         mfDao=new ModelFeatureValueDao(session);
         vmDao= new VehicleModelDao(session);
         vDao=new VehicleDao(session);
+        sDao=new ServiceProviderDAO(session);
+        spDao=new ServiceProviderServicesDao(session);
+        mDao=new MakeDao(session);
+        serviceDao=new ServiceDAO(session);
     }
    
     public List<Object[]> getVehicles()
@@ -57,7 +73,7 @@ public class DataLayer implements Serializable {
         } 
        
    }
-   
+    
    public ArrayList showFeatures(int modelId)
    {
        VehicleModel vm=new VehicleModel();
@@ -66,6 +82,13 @@ public class DataLayer implements Serializable {
        return featuresList;
    }
    
+   public List<Make> showMakes(int d)
+   {
+       ServiceProvider p;
+       p=sDao.find(d);
+       List<Make> makeList= mDao.getMakebyServiceProvider(p.getName());
+       return makeList;
+   }
    public void update(ModelFeaturesValues m)
    {
        session.beginTransaction();
@@ -78,4 +101,28 @@ public class DataLayer implements Serializable {
             HibernateFactory.rollback(transaction);
         }
    }
+   public List<Object[]> findServiceProviders()
+   {
+       List<Object[]> list;
+       list=sDao.getServiceProviderInfo();
+       return list;
+       
+   }
+   public List<ServiceProviderServices> getServices(ServiceProvider serviceProvider)
+   {
+       List<ServiceProviderServices> list=spDao.getByServiceProvider(serviceProvider);
+       return list;
+   }
+   public ServiceProvider findById(int d)
+   {
+       ServiceProvider s=sDao.find(d);
+       return s;
+   }
+   
+   public List<Service> getServices(){
+       List<Service> list=serviceDao.findAll();
+       return list;
+   }
+   
+   
 }
