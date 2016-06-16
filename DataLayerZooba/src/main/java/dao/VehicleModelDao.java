ab1b54b595f52816d67f2e7768c3a7329aab8771
Riham <Riham@Riham-PC>
2016-06-16 15:23:09 -0700

    last edit

--- DataLayerZooba/src/main/java/dao/VehicleModelDao.java
+++ DataLayerZooba/src/main/java/dao/VehicleModelDao.java
@@ -5,21 +5,26 @@
  */
 package dao;
 
+import Exceptions.DataAccessLayerException;
 import abstractDao.AbstractDao;
 import java.util.List;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
+import pojo.Vehicle;
 import pojo.VehicleModel;
 
 /**
  *
  * @author Ehab
  */
-public class VehicleModelDao extends AbstractDao<VehicleModel>{
-    
+public class VehicleModelDao extends AbstractDao<VehicleModel> {
+
     Session session;
-    
+
     public VehicleModelDao(Session s) {
-        super(VehicleModel.class,s);
+        super(VehicleModel.class, s);
         session = s;
     }
 
@@ -46,8 +51,38 @@ public class VehicleModelDao extends AbstractDao<VehicleModel>{
     public void create(VehicleModel t) {
         super.create(t); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
+
+    public List<VehicleModel> findAll() throws DataAccessLayerException {
+         List<VehicleModel> objects = null;
+        try {
+            Query query = session.createQuery("from  VehicleModel");
+            objects = query.list();
+            for(VehicleModel vm:objects)
+            {
+            Hibernate.initialize(vm.getModel());
+             Hibernate.initialize(vm.getTrim());
+              Hibernate.initialize(vm.getYear());
+              Hibernate.initialize(vm.getModel().getMake());
+            }
+        } catch (HibernateException e) {
+            handleException(e);
+        }
+        return objects; }
+
+    public List<VehicleModel> getByVehicleModelId(int id) {
+        String hql = "from VehicleModel v "
+                + "where v.id=?";
+        Query query = session.createQuery(hql).setInteger(0, id);
+        List<VehicleModel> result = query.list();
+        for(VehicleModel v:result)
+        {
+            Hibernate.initialize(v.getModel());
+            Hibernate.initialize(v.getYear());
+            Hibernate.initialize(v.getTrim());
+            Hibernate.initialize(v.getModel().getMake());
+        }
+        return result;
+    }
+
     
 }
--- DataLayerZooba/src/main/java/reham/DataLayer.java
+++ DataLayerZooba/src/main/java/reham/DataLayer.java
@@ -17,6 +17,7 @@ import dao.VehicleModelDao;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import pojo.Make;
@@ -33,8 +34,8 @@ import pojo.VehicleModel;
  */
 public class DataLayer implements Serializable {
 
-   Session session;
-   Vehicle v1=new Vehicle();
+    Session session;
+    VehicleModel v1 = new VehicleModel();
     ModelFeatureValueDao mfDao;
     VehicleModelDao vmDao;
     Transaction transaction;
@@ -43,86 +44,183 @@ public class DataLayer implements Serializable {
     ServiceProviderServicesDao spDao;
     MakeDao mDao;
     ServiceDAO serviceDao;
+
     public DataLayer() {
-        session=HibernateFactory.openSession();
-        mfDao=new ModelFeatureValueDao(session);
-        vmDao= new VehicleModelDao(session);
-        vDao=new VehicleDao(session);
-        sDao=new ServiceProviderDAO(session);
-        spDao=new ServiceProviderServicesDao(session);
-        mDao=new MakeDao(session);
-        serviceDao=new ServiceDAO(session);
+//        mfDao = new ModelFeatureValueDao(session);
+//        vDao = new VehicleDao(session);
+//        sDao = new ServiceProviderDAO(session);
+//        spDao = new ServiceProviderServicesDao(session);
+//        mDao = new MakeDao(session);
+//        serviceDao = new ServiceDAO(session);
+        
     }
-   
-    public List<Object[]> getVehicles()
-   {
-       List<Object[]> vehicles=vDao.getByMake();
-       return vehicles;
-   }
-    public void deleteVehicle(String name)
-   {
-       
-       session.beginTransaction();
-       try{
-       v1=(Vehicle)vDao.getVehicleByName(name);
-       vDao.delete(v1); 
-       session.getTransaction().commit();
-       }
-       catch (DataAccessLayerException ex) {
-            HibernateFactory.rollback(transaction);
-        } 
-       
-   }
-    
-   public ArrayList showFeatures(int modelId)
-   {
-       VehicleModel vm=new VehicleModel();
-       vm=vmDao.find(modelId);
-       ArrayList<ModelFeaturesValues> featuresList= (ArrayList<ModelFeaturesValues>)mfDao.getMFValuesByByVehicleModel(vm);
-       return featuresList;
-   }
-   
-   public List<Make> showMakes(int d)
-   {
-       ServiceProvider p;
-       p=sDao.find(d);
-       List<Make> makeList= mDao.getMakebyServiceProvider(p.getName());
-       return makeList;
-   }
-   public void update(ModelFeaturesValues m)
-   {
-       session.beginTransaction();
-       try{
-       
-       mfDao.saveOrUpdate(m);
-       session.getTransaction().commit();
-       }
-       catch (DataAccessLayerException ex) {
+
+//    public List<Object[]> getVehicles()
+//   {
+//       List<Object[]> vehicles=vDao.getByMake();
+//       return vehicles;
+//   }
+    public List<VehicleModel> getVehicles() {
+        session = HibernateFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        vmDao = new VehicleModelDao(session);
+        List<VehicleModel> vehicles = vmDao.findAll();
+        HibernateFactory.close(session);
+        return vehicles;
+    }
+//    public void deleteVehicle(String name)
+//   {
+//       
+//       session.beginTransaction();
+//       try{
+//       v1=(Vehicle)vDao.getVehicleByName(name);
+//       vDao.delete(v1); 
+//       session.getTransaction().commit();
+//       }
+//       catch (DataAccessLayerException ex) {
+//            HibernateFactory.rollback(transaction);
+//        } 
+//       
+//   }
+
+    public boolean deleteVehicle(int id) {
+        boolean flag = true;
+        int vmId;
+        List<Integer> vmIds=new ArrayList();
+        session = HibernateFactory.openSession();
+        vmDao = new VehicleModelDao(session);
+            vDao=new VehicleDao(session);
+        try {
+            v1 = vmDao.find(id);
+            List<Vehicle> vehicleList=vDao.findAll();
+            for(Vehicle vehicle:vehicleList)
+            {
+                VehicleModel vm=vehicle.getVehicleModel();
+                vmId=vm.getId();
+                vmIds.add(vmId);
+            }
+            for(int n:vmIds)
+            {
+                if(id==n)
+                {
+                flag = false;
+                System.out.println("Can't delete");
+                break;
+                }                
+            }
+            if(flag)
+            {
+                
+                    Transaction transaction = session.beginTransaction();
+                    vmDao.delete(v1);
+                    transaction.commit();
+            }
+        } catch (HibernateException ex) {
+            transaction.rollback();
+        } finally {
+            HibernateFactory.close(session);
+        }
+        return flag;
+    }
+
+    public ArrayList showFeatures(int modelId) {
+        VehicleModel vm = new VehicleModel();
+        session = HibernateFactory.openSession();
+        mfDao = new ModelFeatureValueDao(session);
+        vmDao=new VehicleModelDao(session);
+        vm = vmDao.find(modelId);
+        ArrayList<ModelFeaturesValues> featuresList = (ArrayList<ModelFeaturesValues>) mfDao.getMFValuesByByVehicleModel(vm);
+    //    HibernateFactory.close(session);
+        return featuresList;
+    }
+
+    public List<Make> showMakes(int d) {
+        session = HibernateFactory.openSession();
+        sDao= new ServiceProviderDAO(session);
+        ServiceProvider p;
+        p = sDao.find(d);
+        List<Make> makeList = mDao.getMakebyServiceProvider(p.getName());
+        HibernateFactory.close(session);
+        return makeList;
+    }
+
+    public void update(ModelFeaturesValues m) {
+        session = HibernateFactory.openSession();
+        mfDao = new ModelFeatureValueDao(session);
+        Transaction transaction = session.beginTransaction();
+        try {
+
+            mfDao.saveOrUpdate(m);
+            transaction.commit();
+        } catch (DataAccessLayerException ex) {
             HibernateFactory.rollback(transaction);
         }
-   }
-   public List<Object[]> findServiceProviders()
-   {
-       List<Object[]> list;
-       list=sDao.getServiceProviderInfo();
-       return list;
-       
-   }
-   public List<ServiceProviderServices> getServices(ServiceProvider serviceProvider)
-   {
-       List<ServiceProviderServices> list=spDao.getByServiceProvider(serviceProvider);
-       return list;
-   }
-   public ServiceProvider findById(int d)
-   {
-       ServiceProvider s=sDao.find(d);
-       return s;
-   }
-   
-   public List<Service> getServices(){
-       List<Service> list=serviceDao.findAll();
-       return list;
-   }
-   
-   
+        finally {
+            HibernateFactory.close(session);
+        }
+    }
+
+    public List<Object[]> findServiceProviders() {
+        session = HibernateFactory.openSession();
+        sDao= new ServiceProviderDAO(session);
+        List<Object[]> list;
+        list = sDao.getServiceProviderInfo();
+        HibernateFactory.close(session);
+        return list;
+
+    }
+
+    public List<ServiceProviderServices> getServices(ServiceProvider serviceProvider) {
+        session = HibernateFactory.openSession();
+        spDao = new ServiceProviderServicesDao(session);
+        List<ServiceProviderServices> list = spDao.getByServiceProvider(serviceProvider);
+        HibernateFactory.close(session);
+        return list;
+    }
+
+    public ServiceProvider findById(int d) {
+        session = HibernateFactory.openSession();
+        sDao = new ServiceProviderDAO(session);
+        ServiceProvider s = sDao.find(d);
+        HibernateFactory.close(session);
+        return s;
+    }
+
+    public List<Service> getServices() {
+        session = HibernateFactory.openSession();
+        serviceDao = new ServiceDAO(session);
+        List<Service> list = serviceDao.findAll();
+        HibernateFactory.close(session);
+        return list;
+    }
+
+    public List<VehicleModel> getVehicleModel(int d) {
+        session = HibernateFactory.openSession();
+        vmDao=new VehicleModelDao(session);
+        List<VehicleModel> v = vmDao.getByVehicleModelId(d);
+        HibernateFactory.close(session);
+        return v;
+    }
+    public VehicleModel find(int id)
+    {
+        session = HibernateFactory.openSession();
+        vmDao=new VehicleModelDao(session);
+        VehicleModel v=new VehicleModel();
+        v=vmDao.find(1);
+        return v;
+    }
+
+    public void updateVehicleModel(VehicleModel v) {
+       session = HibernateFactory.openSession();
+        vmDao=new VehicleModelDao(session);
+        Transaction transaction = session.beginTransaction();
+        try {
+            vmDao.saveOrUpdate(v);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+        } finally {
+            HibernateFactory.close(session);
+        }
+    }
 }
